3c51e32316d6d366c4fd79a1a2f9edb7a1e2af10
Arcademic <chavoush.k@gmail.com>
2023-12-12 18:31:40 +0100

    Added infinite room generation

--- scripts/Room.cs
+++ scripts/Room.cs
@@ -1,28 +1,25 @@
 using Godot;
 
-public partial class Room : Node2D
+public partial class Room : Area2D
 {
-	private bool leftOpen;
-	private bool rightOpen;
-	private bool topOpen;
-	private bool bottomOpen;
+	[Signal]
+	public delegate void PlayerEnteredEventHandler(Vector2 pos);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		RayCast2D rayLeft = GetNode<RayCast2D>("ray_left");
-		RayCast2D rayRight = GetNode<RayCast2D>("ray_right");
-		RayCast2D rayUp = GetNode<RayCast2D>("ray_up");
-		RayCast2D rayDown = GetNode<RayCast2D>("ray_down");
-		var spaceState = GetWorld2D().DirectSpaceState;
-		var query = PhysicsRayQueryParameters2D.Create(Vector2.Zero, new Vector2(-350, 0));
-    	var result = spaceState.IntersectRay(query);
-
-		GD.Print(result);
+		BodyEntered += OnBodyEntered;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		EmitSignal(nameof(PlayerEntered), GlobalPosition);
+	}
+
+
 }
